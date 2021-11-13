211982b1bbfe7ad69f10cddc3d980e43c1cb28ad
Rahman <rahman.abdur4420@gmail.com>
2021-11-13 21:28:40 +0600

    add class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './Welcome';
+import Class from './Class'
 
 function App() {
   return (
+    <div>
     <Welcome name="Rahman" age="20"></Welcome>
+    <Class name="Karim"></Class>
+    </div>
+
   );
 }
 
---
+++ src/Class.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Class extends Component {
+    render() {
+        return(
+        <h1>Hello from class component and my name is {this.props.name}</h1>
+        )
+    }
+}
+export default Class;
