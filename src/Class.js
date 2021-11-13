9f6f603a443694a3802f34c201e66cf7cb954b06
Rahman <rahman.abdur4420@gmail.com>
2021-11-13 21:55:31 +0600

    Button click Event

--- src/Class.js
+++ src/Class.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
 class Class extends Component {
+    click(a){
+        alert(a)
+    }
     render() {
         return(
+            <div>
+            <button onClick={this.click.bind(this,"click form class")}>Click From Class</button>
         <h1>Hello from class component and my name is {this.props.name}</h1>
+        </div>
         )
     }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function Welcome(props){
+    function click(a){
+        alert(a)
+    }
     return(
+        <div>
+        <button onClick={click.bind(this,"I am from function")}>Click me</button>
         <h1>Welcome,{props.name} and your age is {props.age}!</h1>
+        </div>
     )
 }
 export default Welcome;
